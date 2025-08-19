9d992528b7ef859591284452b09f94b848da04e7
dharvramoliya <dharvramoliya9@gmail.com>
2025-08-19 23:58:51 +0530

    Create dharvramoliya1.c

---
+++ dharvramoliya1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter your first number:");
+    scanf("%d",&a);
+    printf("enter your second number:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("the sum of %d and %d is %d",a,b,c);
+}
