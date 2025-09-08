965650f39cc08c680315baaecdcd136a77e3db06
dharvramoliya <dharvramoliya9@gmail.com>
2025-09-08 21:44:28 +0530

    loop2.c

---
+++ loop2.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main() {
+    
+    int i;
+  printf("The first 10 odd natural numbers are:\n");
+  for (i = 1; i <= 10; i=i+2) 
+  {
+   printf("%d ", i);
+    }
+  return 0;
+}
