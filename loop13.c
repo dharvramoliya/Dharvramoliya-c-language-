26d468aec36b61ef19f2982681fd059f9395df9c
dharvramoliya <dharvramoliya9@gmail.com>
2025-09-09 20:48:23 +0530

    loop13.c

---
+++ loop13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    printf("Numbers divisible by 13 from 1 to 100:\n");
+
+    for(i = 1; i <= 100; i++) {
+        if(i % 13 == 0) {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
