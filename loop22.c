97eb42f39bb81eab95fae243438ed11ef27de287
dharvramoliya <dharvramoliya9@gmail.com>
2025-09-10 22:19:13 +0530

    loop22.c

---
+++ loop22.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num == 0) {
+        count = 1;
+    } else {
+        for(; num > 0; num = num / 10) {
+            count++;
+        }
+    }
+
+    printf("Total digits = %d\n", count);
+
+    return 0;
+}
