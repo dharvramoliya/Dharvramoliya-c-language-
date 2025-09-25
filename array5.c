ce84fec716db696a6ba43af3436405e41f288bb8
Ramoliya Dharv Vinubhai <dharvramoliya9@gmail.com>
2025-09-25 20:08:35 +0530

    array5.c

---
+++ array5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int a[2][2], b[2][2], c[2][2];
+    int i,j;
+
+    printf("Enter 1st 2x2 matrix:\n");
+    for(i=0;i<2;i++)
+        for(j=0;j<2;j++)
+            scanf("%d",&a[i][j]);
+
+    printf("Enter 2nd 2x2 matrix:\n");
+    for(i=0;i<2;i++)
+        for(j=0;j<2;j++)
+            scanf("%d",&b[i][j]);
+
+    for(i=0;i<2;i++)
+        for(j=0;j<2;j++)
+            c[i][j] = a[i][j] + b[i][j];
+
+    printf("Result :\n");
+    for(i=0;i<2;i++) {
+        for(j=0;j<2;j++)
+            printf("%d ",c[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
